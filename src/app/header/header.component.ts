151b1033cd694a2d36bbdbde869a064e3dcad2fe
dan.benge@gmail.com <dan.benge@gmail.com>
2018-05-24 18:10:00 -0400

    First Commit

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,11 @@
+import { Component, Output, EventEmitter } from '@angular/core';
+
+@Component({
+    selector: 'app-header',
+    templateUrl: './header.component.html'
+})
+export class HeaderComponent{
+
+    @Output() viewChanged = new EventEmitter<string>();
+
+}
---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Recipe } from '../recipe.model';
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://media.defense.gov/2011/Feb/04/2000288219/780/780/0/110201-F-6881R-001.JPG')
+  ]
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
