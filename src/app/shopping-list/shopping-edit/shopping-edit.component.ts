a4915c3b76f2dd9fd54b4942fd14a59ab1533aa3
dan.benge@gmail.com <dan.benge@gmail.com>
2018-05-25 16:10:24 -0400

    Latest Challenge

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, EventEmitter, Output } from '@angular/core';
+import { Ingredient } from '../../shared/ingredient.module';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingEditComponent implements OnInit {
 
+  @ViewChild('amountInput') amountInput;
+  @ViewChild('nameInput') nameInput;
+
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  addIngredient()
+  {
+    const blah = new Ingredient(this.nameInput.value, this.amountInput.value);
+    this.ingredientAdded.emit(blah);
   }
 
 }
